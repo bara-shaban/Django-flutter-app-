6ccfa9f73a8bedcbee318db696e7f2ca8afede75
bara-shaban <barashaban03@gmail.com>
2025-03-11 03:32:08 +0300

    nothing much just added the templates folder and rinderd the html file in the views file insted of httpresponse and used the admin ui to add recoreds insted of using django shell

--- worldtour/asiatoursagency/views.py
+++ worldtour/asiatoursagency/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Tour
 
 def index(request):
-    return HttpResponse("Asia Tours Agency")
+    tours = Tour.objects.all()
+    context = {
+        'tours': tours
+    }
+    return render(request, 'tours/index.html', context)
