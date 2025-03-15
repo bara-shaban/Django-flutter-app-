06d422e4b6473236359b1c9aedeb119e6ee82663
bara-shaban <barashaban03@gmail.com>
2025-03-15 23:51:08 +0300

    nothing much just added to the allowed hosts the AWS ip address so i can run it there and some otther minor stuff

--- worldtour/api/urls.py
+++ worldtour/api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.getData, name='getData'),
+    path('<int:id>/', views.getData, name='getData'),
     path('add/', views.addData, name='addData'),
 ]
--- worldtour/api/views.py
+++ worldtour/api/views.py
@@ -1,13 +1,21 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 from asiatoursagency.models import Tour
 from .serializers import TourSerializer
+from django.http import JsonResponse
 
 @api_view(['GET'])
-def getData(request):
-    items = Tour.objects.all()
-    serializer = TourSerializer(items, many=True)
-    return Response(serializer.data)
+def getData(request,id=None):
+    if id:
+        items = Tour.objects.get(id=id)
+        serializer = TourSerializer(items)
+    else:
+        items = Tour.objects.all()
+        serializer = TourSerializer(items, many=True)
+        
+    return JsonResponse({'Tours':serializer.data},status=status.HTTP_200_OK)
+    
 
 
 @api_view(['POST'])
@@ -15,4 +23,5 @@ def addData(request):
     serializer = TourSerializer(data = request.data)
     if serializer.is_valid():
         serializer.save()
-    return Response(serializer.data)
+        return JsonResponse(serializer.data,status=status.HTTP_201_CREATED)
+    return JsonResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
